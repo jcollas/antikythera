eb683c432af4b8841a13011929a3011f93b061a8
Juan J. Collas <jcollas@moreira.com>
2015-12-23 07:26:39 -0500

    Fix main to conform to ios 7+

--- AntikytheraOpenGLPrototype_v3/main.m
+++ AntikytheraOpenGLPrototype_v3/main.m
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AntikytheraOpenGLPrototypeAppDelegate.h"
 
 int main(int argc, char *argv[]) {
     
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, nil);
-        return retVal;
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AntikytheraOpenGLPrototypeAppDelegate class]));
     }
+
 }
